d0a7bbf52eba20974b25c1282334cac862846a7b
pmmf <pedroferreira.121@gmail.com>
2020-12-21 22:18:39 +0000

    refactor: change default to 2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def power(a):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n", type=int, default=4)
+    parser.add_argument("--n", type=int, default=2)
 
     args = parser.parse_args()
     n = args.n
